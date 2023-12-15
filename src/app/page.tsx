edb803345958f5e0a52cac638371a4e6158d35f7
Kote <144833957+YakinFsilva@users.noreply.github.com>
2023-12-15 12:23:58 -0300

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,35 @@
 "use client";
 import { useState } from "react";
+import axios from "axios";
 
 const initialProdutos = [
- { id: 1, nome: "banana"},
- {id: 2, nome: "pera"},
+  { id: 1, nome: "banana" },
+  { id: 2, nome: "pera" },
 ];
 
-//pra mostrar as informações na tela
 export default function Home() {
   const [produtos, setProdutos] = useState(initialProdutos);
 
-  //botão pra puxar as informações
   async function handleClick() {
-    const response = await fetch("http://192.168.68.154:3000/produtos");
-    const result = await response.json();
-    setProdutos([... produtos, ...result]); //juntou os produtos antigos com os novos
-     console.log(result);
+    try {
+      const response = await axios.get("http://192.168.68.154:3000/produtos");
+      const result = response.data;
+      setProdutos([...produtos, ...result]);
+      console.log(result);
+    } catch (error) {
+      console.error("Erro ao recuperar informações:", error.message);
+
     }
+  }
 
-//retornar um htlm
   return (
-  <main> 
-    <button onClick={handleClick}>recuperar informações</button>
-    <ul>
-      {produtos.map((produtos) => (
-        <li> {produtos.nome} </li>
-      ))}
-    </ul>
-
-
-  </main>)
+    <main>
+      <button onClick={handleClick}>Recuperar informações</button>
+      <ul>
+        {produtos.map((produto) => (
+          <li key={produto.id}>{produto.nome}</li>
+        ))}
+      </ul>
+    </main>
+  );
 }
